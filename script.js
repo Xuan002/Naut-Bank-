fdd2891901f43d2e2fc7cc2595a37ce4ece57f34
Richard Naoki <richardnaoki@hotmail.com>
2023-04-27 02:31:10 -0300

    Login e Cadastro Att 27/04

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
- // PARTE INICIO CPF
- let value_cpf = document.querySelector('#campo_cpf');
+function confereSenha(){
+const senha = document.querySelector('input[name=senha]');
+const confirma = document.querySelector('input[name=confirma]');
 
- value_cpf.addEventListener("keydown", function(e) {
-   if (e.key > "a" && e.key < "z") {
-     e.preventDefault();
-   }
- });
-value_cpf.addEventListener("blur", function(e) {
-     //Remove tudo o que não é dígito
-     let validar_cpf = this.value.replace( /\D/g , "");
+if (confirma.value == senha.value) {
+    confirma.setCustomValidity('');
+}
+  else{
+    confirma.setCustomValidity('As senhas não coincidem');
+  }
+}
 
-     //verificação da quantidade números
-     if (validar_cpf.length==11){
 
-     // verificação de CPF valido
-      var Soma;
-      var Resto;
+function confirm(){
+alert("Cadastrado com Sucesso!!")
 
-      Soma = 0;
-      for (i=1; i<=9; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (11 - i);
-         Resto = (Soma * 10) % 11;
+}
 
-      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-      if (Resto != parseInt(validar_cpf.substring(9, 10)) ) return alert("CPF Inválido!");;
-
-      Soma = 0;
-      for (i = 1; i <= 10; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (12 - i);
-      Resto = (Soma * 10) % 11;
-
-      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-      if (Resto != parseInt(validar_cpf.substring(10, 11) ) ) return alert("CPF Inválido!");;
-
-      //formatação final
-      cpf_final = validar_cpf.replace( /(\d{3})(\d)/ , "$1.$2");
-      cpf_final = cpf_final.replace( /(\d{3})(\d)/ , "$1.$2");
-      cpf_final = cpf_final.replace(/(\d{3})(\d{1,2})$/ , "$1-$2");
-      document.getElementById('campo_cpf').value = cpf_final;
-
-     } else {
-       alert("CPF Inválido! É esperado 11 dígitos numéricos.")
-     }   
-
- })
-
- // PARTE FINAL CPF
