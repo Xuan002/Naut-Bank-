c7a03a6db439d1ba0342b10f5472068b39d63723
juan machado <juanmachado123@hotmail.com>
2023-05-03 18:12:34 -0300

    pagina de login e cadastro feito em jsx

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,23 +2,42 @@ import "./style.css"
 
 const Login = () => {
     return ( 
-        <div id="login">
-            <h1 className="title">Login</h1>
-            <form className="form">
-                <div className="fild">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
-                </div>
-                <div className="fild">
-                    <label htmlFor="password">Senha</label>
-                    <input type="password" name="password" id="password" />
-                </div>
-                <div className="actions">
-                    <button type="submit">Entrar</button>
-                </div>
-            </form>
+        <div class="main_div">
+          <div class="title">Login</div>
+         
+
+          <form action="#">
+            <div class="input_box">
+                
+                <input type="text" placeholder="CPF ou RG" required/>
+                <div class="icon"><i class="fa fa-user"></i></div>
+            </div>
+
+            <div class="input_box">
+              <input type="password" placeholder="Senha" required/>
+              <div class="icon"><i class="fa fa-lock"></i></div>
+            </div>
+
+            <div class="option_div">
+              <div class="check_box">
+                <input type="checkbox"/>
+                <span>Lembrar de mim</span>
+              </div>
+
+            
+            </div>
+
+            <div class="input_box button">
+              <input type="submit" value="Login"/>
+            </div>
+
+            <div class="sign_up">
+              Não se cadastrou ainda? <a href="Cadastro.html">Se Cadastre aqui.</a>
+            </div>
+          </form>
         </div>
-     );
+
+    );
 }
  
 export default Login;
